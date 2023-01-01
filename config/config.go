93302e70d1c3b6d147228c83f435681f6026be94
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-02 00:26:51 +0700

    config added

---
+++ config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	App struct {
+		BaseURL string
+		Port    string
+	}
+
+	Database struct {
+		User     string
+		Password string
+		Host     string
+		Port     string
+		DBName   string
+	}
+
+	ApiKey struct {
+		ApiKeyTokoku string
+	}
+}
+
+func InitConfig() *AppConfig {
+	appConfig := AppConfig{}
+
+	// Load file .env di root menggunakan library godotenv
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+		return &appConfig
+	}
+
+	// App
+	appConfig.App.BaseURL = os.Getenv("APP_BASE_URL")
+	appConfig.App.Port = os.Getenv("APP_PORT")
+
+	// Database
+	appConfig.Database.User = os.Getenv("DB_USER")
+	appConfig.Database.Password = os.Getenv("DB_Password")
+	appConfig.Database.Host = os.Getenv("DB_Host")
+	appConfig.Database.Port = os.Getenv("DB_PORT")
+	appConfig.Database.DBName = os.Getenv("DB_NAME")
+
+	// Api Key
+	appConfig.ApiKey.ApiKeyTokoku = os.Getenv("API_KEY_TOKOKU")
+
+	return &appConfig
+}
